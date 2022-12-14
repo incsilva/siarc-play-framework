888aa5627025f71c028423838dbd1a5cf0d626f2
incsilva <snhrlbr@gmail.com>
2022-12-14 11:54:24 -0300

    validacao de campos de usuario

---
+++ app/controllers/Aparelhos.java
@@ -0,0 +1,61 @@
+package controllers;
+
+import java.util.Collections;
+import java.util.List;
+
+import models.Aparelho;
+import models.Status;
+import play.data.validation.Valid;
+import play.mvc.Controller;
+import play.mvc.With;
+import security.Seguranca;
+
+public class Aparelhos extends Controller {
+
+	public static void form() {
+		render();
+	}
+
+	public static void menu() {
+		render();
+	}
+
+	public static void salvar(@Valid Aparelho aparelho) {
+		long qtd = Aparelho.count("nome = ?1", aparelho.nome);
+		if (validation.hasErrors()) {
+			validation.keep();
+			flash.error("Alguns problemas foram encontrados...");
+		} else {
+			aparelho.save();
+			flash.success("Aparelho cadastrado com sucesso.");
+		}
+		form();
+	}
+
+	public static void editar(Long id) {
+		Aparelho aparelho = Aparelho.findById(id);
+		renderTemplate("Aparelhos/form.html", aparelho);
+	}
+
+	public static void remover(Long id) {
+		Aparelho ap = Aparelho.findById(id);
+		ap.delete();
+		flash.success("Aparelho removido com sucesso!");
+		listar();
+	}
+
+	public static void listar() {
+		String termo = params.get("termo");
+
+		List<Aparelho> aparelhos = Collections.EMPTY_LIST;
+		if (termo == null || termo.isEmpty()) {
+			aparelhos = Aparelho.findAll();
+		} else {
+			aparelhos = Aparelho.find(
+					"(lower(nome) like ?1 OR lower(enderecoIp) like ?2 OR lower(enderecoMac) like ?3 OR lower(local) like ?4)",
+					"%" + termo.toLowerCase() + "%", "%" + termo.toLowerCase() + "%", "%" + termo.toLowerCase() + "%",
+					"%" + termo.toLowerCase() + "%").fetch();
+		}
+		render(aparelhos, termo);
+	}
+}
---
+++ app/models/Usuario.java
@@ -0,0 +1,95 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Transient;
+
+import models.statusUsuario;
+import net.sf.oval.constraint.EqualToField;
+import play.data.validation.Email;
+import play.data.validation.Equals;
+import play.data.validation.Min;
+import play.data.validation.Password;
+import play.data.validation.Required;
+import play.data.validation.Unique;
+import play.db.jpa.Model;
+
+@Entity
+public class Usuario extends Model {
+
+	@Required(message = "Este campo é obrigatório")
+	@Unique(message = "Este nome já foi cadastrado no sistema")
+	public String nome;
+
+	@Email(message = "O e-mail deve estar no formato correto.")
+	@Unique(message = "O e-mail informado já está em uso. Tente outro")
+	@Required(message = "Este campo é obrigatório")
+	public String email;
+
+	@Required(message = "Este campo é obrigatório")
+	@Unique(message = "A matrícula informada já está cadastrada no sistema")
+	@Min(value = 8, message = "Sua matricula deve ter no mínimo 8 caracteres")
+	public String matricula;
+	
+	@Required(message = "Este campo é obrigatório")
+	@Password
+	@Equals("confirmaSenha")
+	public String senha;
+
+	@Transient
+	public String confirmaSenha;
+
+	@Enumerated(EnumType.STRING)
+	public Funcao funcao;
+	
+	@Enumerated(EnumType.STRING)
+	public statusUsuario status;
+
+	public Usuario() {
+		funcao = funcao.usuario;
+		status = statusUsuario.ativo;
+	}
+	
+	public void ativar() {
+		status = statusUsuario.ativo;
+	}
+	
+	public void inativar() {
+		status = statusUsuario.inativo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public Funcao getFuncao() {
+		return funcao;
+	}
+
+	public void setFuncao(Funcao funcao) {
+		this.funcao = funcao;
+	}
+	
+	
+}
